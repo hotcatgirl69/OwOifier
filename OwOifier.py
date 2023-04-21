1ec6b1d21c30795937f6e8df6f2937ead7964782
hotcatgirl69 <122986190+hotcatgirl69@users.noreply.github.com>
2023-04-21 10:09:48 -0400

    v1.2.0
    Changed the GUI to a horizontal layout.
    Changed Lorem Ipsum button to pick a random Copypasta from settings.
    Split the Widgets into individual functions.
    Changed the color of the textboxes.
    Got stutter, prefix, and stutter to work again.

--- OwOifier.py
+++ OwOifier.py
@@ -1,29 +1,27 @@
-from json import *
+from json import load
 from random import choice, randint
+
 from customtkinter import *
 from pyperclip import copy
-from lorem import sentence
-from json import load
 
 
 class owoifier:
-    def __init__(self, inputText='', LRToWs=True, YAfterNs=True, repeatAfterYs=True, replaceWords=True, stutterChance=15, prefixChance=15, suffixChance=15) -> None:
+    def __init__(self, inputText='', LW=True, YN=True, repeat=True, replace=True, stutterChance=15, prefixChance=15, suffixChance=15) -> None:
         self.inputText = inputText
-        self.LRToWs = LRToWs
-        self.YAfterNs = YAfterNs
-        self.repeatAfterYs = repeatAfterYs
-        self.replaceWords = replaceWords
+        self.LW = LW
+        self.YN = YN
+        self.repeat = repeat
+        self.replace = replace
         self.stutterChance = stutterChance
         self.prefixChance = prefixChance
         self.suffixChance = suffixChance
 
 		# TODO: import settings from settings.json
-        with open('settings.json', 'r') as data:
-            settings = load(data)
-        self.replacements = settings["replacements"]
-        self.prefixes = settings["prefixes"]
-        self.suffixes = settings["suffixes"]
-        self.noTranslations = settings["noTranslations"]
+        with open('settings.json', 'r') as data: settings: dict = load(data)
+        self.replacements: dict[str, str] = settings["replacements"]
+        self.prefixes: list[str] = settings["prefixes"]
+        self.suffixes: list[str] = settings["suffixes"]
+        self.noTranslations: list[str] = settings["noTranslations"]
 
     def owoify(self) -> str:
         self.text = [line.split() for line in self.inputText.splitlines()]
@@ -31,22 +29,18 @@ class owoifier:
             self.word = 0
             while self.word < len(self.text[self.line]):
                 alpha = self.findAlpha()
-                if 1 not in alpha:
-                    self.word += 1
-                    continue
-                if self.replaceWords: self.replaceWord()
+                if 1 not in alpha: self.word += 1; continue
+                if self.replace: self.replaceWord()
                 self.dontTranslate()
                 self.escape = False
                 self.removeEscape()
-                if self.escape:
-                    self.word += 1
-                    continue
-                if self.LRToWs: self.LRToW()
-                if self.YAfterNs: self.YAfterN()
-                if self.repeatAfterYs: self.repeatAfterY()
-                if 0 < self.stutterChance < 1 and 0 not in alpha: self.addStutter()
-                if 0 < self.prefixChance  < 1: self.addPrefix()
-                if 0 < self.suffixChance  < 1: self.addSuffix()
+                if self.escape: self.word += 1; continue
+                if self.LW: self.LRToW()
+                if self.YN: self.YAfterN()
+                if self.repeat: self.repeatAfterY()
+                if 0 != self.stutterChance and 0 not in alpha: self.addStutter()
+                if 0 != self.prefixChance: self.addPrefix()
+                if 0 != self.suffixChance: self.addSuffix()
                 self.word += 1
         return '\n'.join([' '.join(line) for line in self.text])
 
@@ -56,9 +50,12 @@ class owoifier:
     def replaceWord(self) -> None:
         word = self.text[self.line][self.word]
         if word.lower() in self.replacements: 
-            if word.istitle(): self.text[self.line][self.word] = f'${self.replacements[word.lower()].title()}$'
-            elif word.isupper(): self.text[self.line][self.word] = f'${self.replacements[word.lower()].upper()}$'
-            else: self.text[self.line][self.word] = f'${self.replacements[word]}$'
+            if word.istitle(): 
+                self.text[self.line][self.word] = f'${self.replacements[word.lower()].title()}$'
+            elif word.isupper(): 
+                self.text[self.line][self.word] = f'${self.replacements[word.lower()].upper()}$'
+            else: 
+                self.text[self.line][self.word] = f'${self.replacements[word]}$'
 
     def dontTranslate(self) -> None:
         word = self.text[self.line][self.word]
@@ -67,15 +64,12 @@ class owoifier:
 
     def removeEscape(self) -> None:
         word = self.text[self.line][self.word]
-        escape = word.startswith('$') and word.endswith('$')
+        escape: bool = word.startswith('$') and word.endswith('$')
         if escape: self.text[self.line][self.word] = word.replace('$', '')
         self.escape = escape
 
     def LRToW(self) -> None:
-        word = self.text[self.line][self.word]
-        word = word.replace('l', 'w')
-        word = word.replace('r', 'w')
-        self.text[self.line][self.word] = word
+        self.text[self.line][self.word] = self.text[self.line][self.word].replace('l', 'w').replace('r', 'w')
 
     def YAfterN(self) -> None:
         word = self.text[self.line][self.word]
@@ -94,157 +88,136 @@ class owoifier:
 
     def addStutter(self) -> None:
         word = self.text[self.line][self.word]
-        stutter, step, increaseChance = [], 1, 0
-        while randint(1, 100) > self.stutterChance:
-            if randint(1, 10) <= increaseChance:
-                step += 1
-                increaseChance = 0
-            increaseChance += 1
+        stutter = []
+        step, increaseChance = 1, 0
+        while randint(1, 100) < self.stutterChance:
+            if randint(1, 10) <= increaseChance: step += 1; increaseChance = 0
             if step > len(word): break
             stutter.append(word[:step])
+            increaseChance += 1
         stutter.append(word)
         self.text[self.line][self.word] = '-'.join(stutter)
 
     def addPrefix(self) -> None:
-        line, word = self.text[self.line], self.text[self.line][self.word]
-        if randint(1, 100) <= self.prefixChance:
-            line.insert(word-1, choice(self.prefixes))
-        self.text[self.line], self.text[self.line][self.word] = line, word+1
+        word = self.text[self.line][self.word]
+        if randint(1, 100) < self.prefixChance:
+            word = f"{choice(self.prefixes)} {word}"
+        self.text[self.line][self.word] = word
 
     def addSuffix(self) -> None:
-        line, word = self.text[self.line], self.text[self.line][self.word]
-        if randint(1, 100) <= self.suffixChance:
-            line.insert(word+1, choice(self.suffixes))
-        self.text[self.line], self.text[self.line][self.word] = line, word+2
+        word = self.text[self.line][self.word]
+        if randint(1, 100) < self.suffixChance:
+            word = f"{word} {choice(self.suffixes)}"
+        self.text[self.line][self.word] = word
 
 
 class App(CTk):
     def __init__(self) -> None:
         super().__init__()
-        self.protocol("WM_DELETE_WINDOW", self.on_close)
-        self.owo = owoifier()
         
         # app settings
         self.title("OwOifier")
-        self.geometry(f'{440}x{720}')
+        self.minsize(700, 350)
         set_appearance_mode("system")
         set_default_color_theme("blue")
-        self.configure(fg_color=("#ddb6dc", "#1f1f1f"))
+        self.configure(fg_color=("#ddb6dc", "#333333"))
         
-        # TODO: move all frame into separate classes
-        # create widgets
-        # text
-        self.textFrame = CTkFrame(self)
-        self.inputBox = CTkTextbox(self.textFrame, wrap=WORD)
-        self.output = CTkTextbox(self.textFrame, wrap=WORD, state=DISABLED)
-        self.copyButton = CTkButton(self.textFrame, text="Copy Output", command=self.copyText)
-        self.loremIpsumButton = CTkButton(self.textFrame, text="Lorem Ipsum", command=self.loremIpsum)
-        # menu
-        self.menuFrame = CTkFrame(self)
+        # widgets
+        self.owo = owoifier()
+        self.widgetsInit()
+        self.setDefaults()
+        self.setColors()
+        self.setGrid()
+        self.placeWidgets()
+    
+    # widget functions
+    def widgetsInit(self) -> None:
+        self.inputButton = CTkButton(self, text="OwOify!!", command=self.updateText)
+        # input
+        self.inputBox = CTkTextbox(self, wrap=WORD)
+        self.pastaButton = CTkButton(self, text="Random Copypasta", command=self.pasta)
+        # output
+        self.outputBox = CTkTextbox(self, wrap=WORD, state=DISABLED)
+        self.copyButton = CTkButton(self, text="Copy Output", command=self.copyText)
         # slider
-        self.sliderFrame = CTkFrame(self.menuFrame)
-        self.stutterSlider = CTkSlider(self.sliderFrame, from_=0, to=100, number_of_steps=100, command=self.updateStutter)
-        self.prefixSlider = CTkSlider(self.sliderFrame, from_=0, to=100, number_of_steps=100, command=self.updatePrefix)
-        self.suffixSlider = CTkSlider(self.sliderFrame, from_=0, to=100, number_of_steps=100, command=self.updateSuffix)
+        self.sliderFrame = CTkFrame(self)
         self.stutterLabel = CTkLabel(self.sliderFrame)
+        self.stutterSlider = CTkSlider(self.sliderFrame, from_=0, to=100, number_of_steps=100, command=self.updateStutter)
         self.prefixLabel = CTkLabel(self.sliderFrame)
+        self.prefixSlider = CTkSlider(self.sliderFrame, from_=0, to=100, number_of_steps=100, command=self.updatePrefix)
         self.suffixLabel = CTkLabel(self.sliderFrame)
+        self.suffixSlider = CTkSlider(self.sliderFrame, from_=0, to=100, number_of_steps=100, command=self.updateSuffix)
         # checkbox
-        self.checkboxFrame = CTkFrame(self.menuFrame)
-        self.LRToWCheck = CTkCheckBox(self.checkboxFrame, text="L's and R's Converted to W's", command=self.updateLRToW)
-        self.YAfterNCheck = CTkCheckBox(self.checkboxFrame, text="Vowels After N's Become Y's", command=self.updateYAfterN)
-        self.repeatAfterYCheck = CTkCheckBox(self.checkboxFrame, text="Repeat Words Ending with Y", command=self.updateRepeatAfterY)
-        self.replaceWordsCheck = CTkCheckBox(self.checkboxFrame, text="Replace Words", command=self.updateReplaceWords)
-        # input button
-        self.inputButton = CTkButton(self.menuFrame, text="OwOify!!", command=self.updateText)
+        self.checkboxFrame = CTkFrame(self)
+        self.LWCheck = CTkCheckBox(self.checkboxFrame, text="L's and R's Converted to W's", command=self.updateLW)
+        self.YNCheck = CTkCheckBox(self.checkboxFrame, text="Vowels After N's Become Y's", command=self.updateYN)
+        self.repeatCheck = CTkCheckBox(self.checkboxFrame, text="Repeat Words Ending with Y", command=self.updateRepeat)
+        self.replaceCheck = CTkCheckBox(self.checkboxFrame, text="Replace Words", command=self.updateReplace)
 
-        # set defaults
-        # set slider defaults
-        self.stutterSlider.set(15)
-        self.prefixSlider.set(15)
-        self.suffixSlider.set(15)
-        self.updateStutter(self.stutterSlider.get())
-        self.updateSuffix(self.prefixSlider.get())
-        self.updatePrefix(self.suffixSlider.get())
-        # checkboxes are checked by default
-        self.LRToWCheck.select()
-        self.YAfterNCheck.select()
-        self.repeatAfterYCheck.select()
-        self.replaceWordsCheck.select()
+    def setDefaults(self) -> None:
+        self.stutterSlider.set(15); self.prefixSlider.set(15); self.suffixSlider.set(15)
+        self.updateStutter(self.stutterSlider.get()); self.updateSuffix(self.prefixSlider.get()); self.updatePrefix(self.suffixSlider.get())
+        self.LWCheck.select(); self.YNCheck.select(); self.repeatCheck.select(); self.replaceCheck.select()
 
-        # setting colors
-        self.menuFrame.configure(fg_color=("#ddb6dc", "#1f1f1f"))
-        self.textFrame.configure(fg_color="transparent")
+    def setColors(self) -> None:
         self.sliderFrame.configure(fg_color="transparent")
         self.checkboxFrame.configure(fg_color="transparent")
+        self.inputBox.configure(fg_color="#4D4D4D")
+        self.outputBox.configure(fg_color="#4D4D4D")
         self.inputButton.configure(fg_color="#9e2e96", hover_color="#7f2478")
         self.copyButton.configure(fg_color="#9e2e96", hover_color="#7f2478")
-        self.loremIpsumButton.configure(fg_color="#9e2e96", hover_color="#7f2478")
+        self.pastaButton.configure(fg_color="#9e2e96", hover_color="#7f2478")
+        self.LWCheck.configure(fg_color="#9e2e96", hover_color="#7f2478")
+        self.YNCheck.configure(fg_color="#9e2e96", hover_color="#7f2478")
+        self.repeatCheck.configure(fg_color="#9e2e96", hover_color="#7f2478")
+        self.replaceCheck.configure(fg_color="#9e2e96", hover_color="#7f2478")
         self.stutterSlider.configure(button_color="#9e2e96", progress_color="#9e2e96", button_hover_color="#7f2478")
         self.prefixSlider.configure(button_color="#9e2e96", progress_color="#9e2e96", button_hover_color="#7f2478")
         self.suffixSlider.configure(button_color="#9e2e96", progress_color="#9e2e96", button_hover_color="#7f2478")
-        self.LRToWCheck.configure(fg_color="#9e2e96", hover_color="#7f2478")
-        self.YAfterNCheck.configure(fg_color="#9e2e96", hover_color="#7f2478")
-        self.repeatAfterYCheck.configure(fg_color="#9e2e96", hover_color="#7f2478")
-        self.replaceWordsCheck.configure(fg_color="#9e2e96", hover_color="#7f2478")
 
-        # placing widgets in grid layout
-        # text
-        self.textFrame.grid(row=0, column=0, columnspan=2, padx=20, pady=20, sticky=NSEW)
-        self.inputBox.grid(row=0, column=0, columnspan=2, pady=(0, 20), sticky=NSEW)
-        self.output.grid(row=1, column=0, columnspan=2, sticky=NSEW)
-        self.loremIpsumButton.grid(row=2, column=0, sticky=W)
-        self.copyButton.grid(row=2, column=1, padx=20, sticky=E)
-        # menu
-        self.menuFrame.grid(row=1, column=0, columnspan=2, padx=20, pady=(0, 20))
-        # slider
-        self.sliderFrame.grid(row=0, column=0, columnspan=2)
-        self.stutterLabel.grid(row=0, column=0, sticky=NSEW)
-        self.prefixLabel.grid(row=0, column=1, sticky=NSEW)
-        self.suffixLabel.grid(row=0, column=2, sticky=NSEW)
-        self.stutterSlider.grid(row=1, column=0, sticky=NSEW)
-        self.prefixSlider.grid(row=1, column=1, sticky=NSEW)
-        self.suffixSlider.grid(row=1, column=2, sticky=NSEW)
-        # checkbox
-        self.checkboxFrame.grid(row=1, column=0, pady=(10, 0), sticky=W)
-        self.LRToWCheck.grid(row=0, column=0, padx=(0, 10), pady=(20, 10), sticky=W)
-        self.YAfterNCheck.grid(row=1, column=0, padx=(0, 10), pady=(0, 10), sticky=W)
-        self.repeatAfterYCheck.grid(row=2, column=0, padx=(0, 10), pady=(0, 10), sticky=W)
-        self.replaceWordsCheck.grid(row=3, column=0, padx=(0, 10), sticky=W)
-        self.inputButton.grid(row=1, column=1, padx=(10, 0), pady=(10, 0), sticky=NSEW)
-
-        # allow for resizable widgets
-        # root
+    def setGrid(self) -> None:
         self.rowconfigure(0, weight=1)
         self.rowconfigure(1, weight=1)
         self.columnconfigure(0, weight=1)
-        self.columnconfigure(1, weight=1)
-        # textFrame
-        self.textFrame.rowconfigure(0, weight=1)
-        self.textFrame.rowconfigure(1, weight=1)
-        self.textFrame.columnconfigure(0, weight=1)
-        # menuFrame
-        self.menuFrame.rowconfigure(1, weight=1)
-        self.menuFrame.columnconfigure(1, weight=1)
-        # sliderFrame is scalable horizontally only
-        self.sliderFrame.columnconfigure(0, weight=1)
-        self.sliderFrame.columnconfigure(1, weight=1)
-        self.sliderFrame.columnconfigure(2, weight=1)
+        self.columnconfigure(2, weight=1)
+
+    def placeWidgets(self) -> None:
+        self.configure(padx=15, pady=15)
+        self.inputBox.grid(     row=0, column=0, rowspan=2,                            sticky=NSEW)
+        self.pastaButton.grid(  row=2, column=0,            pady=(10, 0),              sticky=W)
+        self.checkboxFrame.grid(row=0, column=1,            padx=10,      pady=(5, 0), sticky=NSEW)
+        self.sliderFrame.grid(  row=1, column=1,            padx=10)
+        self.inputButton.grid(  row=2, column=1,            pady=(10, 0))
+        self.outputBox.grid(    row=0, column=2, rowspan=2,                            sticky=NSEW)
+        self.copyButton.grid(   row=2, column=2,            pady=(10, 0),              sticky=E)
+        # > checkbox
+        self.LWCheck.grid(      row=0,                      pady=(0, 10),              sticky=W)
+        self.YNCheck.grid(      row=1,                      pady=(0, 10),              sticky=W)
+        self.repeatCheck.grid(  row=2,                      pady=(0, 10),              sticky=W)
+        self.replaceCheck.grid( row=3,                      pady=(0, 10),              sticky=W)
+        # > slider
+        self.stutterLabel.grid( row=0)
+        self.stutterSlider.grid(row=1,                      pady=(0, 10))
+        self.prefixLabel.grid(  row=2)
+        self.prefixSlider.grid( row=3,                      pady=(0, 10))
+        self.suffixLabel.grid(  row=4)
+        self.suffixSlider.grid( row=5,                      pady=(0, 10))
 
     # textbox functions
     def updateText(self) -> None:
         self.owo.inputText = self.inputBox.get('0.0', END)
-        self.output.configure(state=NORMAL)
-        self.output.delete('0.0', END)
-        self.output.insert(INSERT, self.owo.owoify())
-        self.output.configure(state=DISABLED)
+        self.outputBox.configure(state=NORMAL)
+        self.outputBox.delete('0.0', END)
+        self.outputBox.insert(INSERT, self.owo.owoify())
+        self.outputBox.configure(state=DISABLED)
 
     def copyText(self) -> None:
-        copy(self.output.get('0.0', END))
+        copy(self.outputBox.get('0.0', END))
 
-    def loremIpsum(self) -> None:
+    def pasta(self) -> None:
         self.inputBox.delete('0.0', END)
-        self.inputBox.insert('0.0', '\n'.join([sentence() for i in range(10)]))
+        with open("settings.json") as data: settings: dict = load(data)
+        self.inputBox.insert('0.0', choice(settings["pasta"]))
         self.updateText()
 
     # slider functions
@@ -261,21 +234,17 @@ class App(CTk):
         self.suffixLabel.configure(text=f'Suffix Chance: {val:.0f}%')
 
     # checkbox functions
-    def updateLRToW(self) -> None:
-        self.owo.LRToWs = bool(self.LRToWCheck.get())
+    def updateLW(self) -> None:
+        self.owo.LW = bool(self.LWCheck.get())
 
-    def updateYAfterN(self) -> None:
-        self.owo.YAfterNs = bool(self.YAfterNCheck.get())
+    def updateYN(self) -> None:
+        self.owo.YN = bool(self.YNCheck.get())
 
-    def updateRepeatAfterY(self) -> None:
-        self.owo.repeatAfterYs = bool(self.repeatAfterYCheck.get())
+    def updateRepeat(self) -> None:
+        self.owo.repeat = bool(self.repeatCheck.get())
 
-    def updateReplaceWords(self) -> None:
-        self.owo.replaceWords = bool(self.replaceWordsCheck.get())
-
-    # root functions
-    def on_close(self) -> None:
-        self.destroy()
+    def updateReplace(self) -> None:
+        self.owo.replace = bool(self.replaceCheck.get())
 
 
 if __name__ in "__main__":
